986c3570653d7bd392a96f1ed20e63e5a23a4db2
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-05-27 19:36:17 +0500

    Add preset change method
    Modify file for tip_percent buttons

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 const bill_amount = document.getElementById("bill_amount");
 const currency = document.getElementById("currency");
 const tip_percent = document.getElementById("tip_percent");
+const tip_button_1 = document.getElementById("tip_button_1");
+const tip_button_2 = document.getElementById("tip_button_2");
+const tip_button_3 = document.getElementById("tip_button_3");
 const number_of_people = document.getElementById("number_of_people");
 const result_total_tip = document.getElementById("result_total_tip");
 const result_total_amount = document.getElementById("result_total_amount");
@@ -11,31 +14,54 @@ const result_pay_per_person = document.getElementById("result_pay_per_person");
 
 // Implementing a function to calculate
 function calculate() {
-    const get_bill_amount = parseFloat(bill_amount.value) || -1; // Adding || (OR operator) to make sure that if user enters any string or invalid integer, the program will raise the error
-    if (get_bill_amount < 0) { //Conditional Statement
-
-    }
-    const get_tip_percent = parseFloat(tip_percent.value) / 100.0 || -1;
-    if (get_tip_percent < 0) {
-
-    }
-    const get_number_of_people = parseInt(number_of_people.value) || -1;
-    if (get_number_of_people < 0) {
-
-    }
+    const get_bill_amount = parseFloat(bill_amount.value) || 0; // Adding || (OR operator) to make sure that if user enters any string or invalid integer, the program will raise the error    
+    const get_tip_percent = parseFloat(tip_percent.value) / 100.0 || 0;
+    const get_number_of_people = parseInt(number_of_people.value) || 0;
     const get_currency_value = currency.value;
 
-    const total_tip = get_tip_percent * get_bill_amount * get_currency_value;
-    const total_bill = total_tip + get_bill_amount;
-    const pay_per_person = (total_bill / get_number_of_people);
+    const total_tip = get_bill_amount * get_tip_percent;
+    const total_amount = get_bill_amount + total_tip;
+    const pay_per_person = total_amount / get_number_of_people;
 
-    result_total_tip.textContent = total_tip;
-    result_total_amount.textContent = total_bill;
+    result_total_tip.textContent = total_tip + get_currency_value;
+    result_total_amount.textContent = total_amount;
     result_pay_per_person.textContent = pay_per_person;
 }
 
 // Adding event listeners to call the calculate function
 bill_amount.addEventListener("input", calculate);
+
+//Function to change preset of tip_percent button
+function change_tip_percent_button_preset(tip_value) {
+
+    if (tip_value != 10) {
+        document.getElementById("tip_button_1").style.backgroundColor = "";
+    }
+    if (tip_value != 15) {
+        document.getElementById("tip_button_2").style.backgroundColor = "";
+    }
+    if (tip_value != 20) {
+        document.getElementById("tip_button_3").style.backgroundColor = "";
+    }
+
+    this.style.backgroundColor = "green";
+    tip_percent.value = tip_value;
+    calculate();
+}
+
+document.getElementById("tip_button_1").addEventListener("click", function () {
+    change_tip_percent_button_preset.call(this, 10);
+});
+
+document.getElementById("tip_button_2").addEventListener("click", function () {
+    change_tip_percent_button_preset.call(this, 15);
+});
+
+document.getElementById("tip_button_3").addEventListener("click", function () {
+    change_tip_percent_button_preset.call(this, 20);
+});
+
 tip_percent.addEventListener("input", calculate);
+
 number_of_people.addEventListener("input", calculate);
 currency.addEventListener("change", calculate);
