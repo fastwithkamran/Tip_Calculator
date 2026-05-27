232e0d7866f61cf9f9505003fa8d88ab00b18b73
Kamran Ayaz <224069719+fastwithkamran@users.noreply.github.com>
2026-05-27 16:59:35 +0500

    Declared & Initialized constant variables in script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// File for backend calculation
+// Declaring and Initializing variables using DOM
+// Using constants variables so I accidentally not change it later
+const bill_amount=document.getElementById("bill_amount");
+const currency=document.getElementById("currency");
+const tip_percent=document.getElementById("tip_percent");
+const number_of_people=document.getElementById("number_of_people");
+const result=document.getElementById("result");
